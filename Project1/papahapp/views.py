997059177ad5271138d3112e06edce609799c3de
allipope <allipope21@gmail.com>
2022-10-28 13:29:06 -0600

    form view

--- Project1/papahapp/views.py
+++ Project1/papahapp/views.py
@@ -5,4 +5,4 @@ def indexPageView(request) :
     return HttpResponse("This is our main page")
 
 def formPageView(request) : 
-    return HttpResponse("This is our forms page")
+    return HttpResponse("This is a page with the form that people can fill out to report phishing.")
