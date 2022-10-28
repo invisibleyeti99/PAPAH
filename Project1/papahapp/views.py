b44517b18c580f93f4d0eee37a908a09417817ef
Nathan Kunz <nathanck@byu.edu>
2022-10-28 13:26:40 -0600

    Changes to be committed:giddy up test 2
    modified:   Project1/papahapp/views.py

--- Project1/papahapp/views.py
+++ Project1/papahapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def indexPageView(request) : 
-    return HttpResponse("This is our main page talking about Phishing and Hacking")
+    return HttpResponse("This is our main page talking about Phishing and Hacking this is nathans test number 2")
 
 def formPageView(request) : 
     return HttpResponse("This is our forms page")
