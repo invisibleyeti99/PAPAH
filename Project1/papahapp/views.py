58eaf4c370d41d26c5703d6932b513778c51b00c
Nathan Kunz <nathanck@byu.edu>
2022-10-28 13:21:59 -0600

    Changed the title and test 1

--- Project1/papahapp/views.py
+++ Project1/papahapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def indexPageView(request) : 
-    return HttpResponse("This is our main page")
+    return HttpResponse("This is our main page talking about Phishing and Hacking")
 
 def formPageView(request) : 
     return HttpResponse("This is our forms page")
