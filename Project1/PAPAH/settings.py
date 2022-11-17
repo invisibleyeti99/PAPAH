aa5d255dfbf68722137aed5117d4613730f6e2ef
Collin Young <ctyoung1@byu.edu>
2022-11-17 08:52:26 -0700

    Changed the database in settings.py

--- Project1/PAPAH/settings.py
+++ Project1/PAPAH/settings.py
@@ -74,12 +74,18 @@ WSGI_APPLICATION = 'PAPAH.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+#Database stuff just needs to be tested
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'Papah',
+    'USER': 'postgres',
+    'PASSWORD': 'Culasi1541?',
+    'HOST': 'localhost',
+    'PORT': '5433'
     }
-}
+} 
 
 
 # Password validation
