22ac1a2e8b1dae0c0d88002ec3da724fa23b796d
allipope <allipope21@gmail.com>
2022-12-09 13:04:26 -0700

    changed the form and added the table

--- Project1/papahapp/views.py
+++ Project1/papahapp/views.py
@@ -4,22 +4,22 @@ from .forms import *
 from .models import *
 
 def indexPageView(request) : 
-
     return render(request, 'papahapp/index.html')
+
 
 def formPageView(request) : 
     data = Breach.objects.all()
+    #breachs = request.user.breachs_set.all() dunno why this isn't working, but we have to define breach to be able to use it in the context
     if request.method == 'POST':
         form = BreachForm(request.POST,)
         if form.is_valid():
             form.save()
     else:
         form = BreachForm()
-        context = {
+    context = {
         'data': data,
         'form': form,
-
+        #'breachs' : breachs,
     }
-   
-        return render(request, 'papahapp/form.html', context)#28.08 on video
+    return render(request, 'papahapp/form.html', context)
 
