0e8c5673e75c4724f58c393a69c5547c31f4dc78
invisibleyeti99 <mattjdavies199@gmail.com>
2022-12-12 20:45:48 -0700

    commit

--- Project1/papahapp/views.py
+++ Project1/papahapp/views.py
@@ -4,6 +4,19 @@ from .forms import *
 from .models import *
 
 def indexPageView(request) : 
+    data = Breach.objects.all()
+    #breachs = request.user.breachs_set.all() dunno why this isn't working, but we have to define breach to be able to use it in the context
+    if request.method == 'POST':
+        form = BreachForm(request.POST,)
+        if form.is_valid():
+            form.save()
+    else:
+        form = BreachForm()
+    context = {
+        'data': data,
+        'form': form,
+        #'breachs' : breachs,
+    }
     return render(request, 'papahapp/index.html')
 
 
